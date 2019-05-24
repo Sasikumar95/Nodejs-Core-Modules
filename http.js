41c4db8a8728a5de96d61e04b1ecbd9c97171aec
SasikumarC95 <sasimymails@gmail.com>
2019-05-24 20:25:38 +0530

    http module

--- http.js
+++ http.js
@@ -1,9 +1,21 @@
-var http = require('http');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-var server = http.createServer(function(req, res){
-    res.end("Server Started");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+      res.write(data);
+      res.end();
+    });
+  }
+
+  if(req.url==='/user'){
+      res.write("<h1>Welcome Sasikumar</h1>");
+      res.end();
+  }
 });
 
-server.listen(1234,'localhost',function(){
-    console.log("server is started port on : 1234");
+server.listen(3000, "localhost", () => {
+  console.log("server is started port on : 3000");
 });
